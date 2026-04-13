63993408dada71c997dbcb12a3ece41a4e5bba9f
chrisjrooney <chrisrooney1@gmail.com>
2026-04-13 14:12:56 +0000

    Deploying to gh-pages from @ chrisjrooney/my-website@eb86b897a0d96244a69cd9c3a67f0b064532cc60 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,28 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/my-website/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/my-website/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/my-website/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/my-website/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
